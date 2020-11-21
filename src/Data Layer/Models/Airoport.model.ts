d5124f05fda872e6c775e8be62837bef04bfd0bc
JonnHenry <jonnathanca9601@gmail.com>
2020-11-21 15:36:26 -0500

    Create a Model Airoport

--- src/Data Layer/Models/Airoport.model.ts
+++ src/Data Layer/Models/Airoport.model.ts
@@ -1,51 +1,126 @@
 import { Schema, model} from 'mongoose';
-import IClase from '../interfaces/IClase';
-//TODO: The interface is not define yet
+import IAiroport from "../../Interfaces/Airoport";
 
 
-const claseSchema = new Schema({
-    
-    titulo: {
+const AiroportSchema = new Schema({
+    Year: {
         type: String,
-        required: [ true, 'El nombre es necesario' ]
+        required: [ true, 'El año es necesario.' ]
     },
-    descripcion: {
-        type: String,
-        default: ''
-    },
-    id: {
+    Month: {
         type: Number,
-        unique: true,
-        required: [ true, 'El correo es necesario' ]
+        required: [ true, 'El mes es necesario.' ]
     },
-    imagenClase:{
-        type: String,
-        required: [ true, 'La imagen de la clase es necesaria para la clase' ]
-    },
-    nombreVideo:{
-        type: String,
-        required: [ true, 'El video es necesario para la clase' ]
-    },
-    calificacion:{
+    DayofMonth: {
         type: Number,
-        default: 5
+        required: [ true, 'El dia del mes es necesario.' ]
     },
-    comentarios: [
-        {
-            usuario: {
-                type: Schema.Types.ObjectId,
-                ref: 'Usuario',
-                required: [ true, 'Debe de existir una referencia a un usuario' ]
-            },
-            comentario: {
-                type: String,
-                default: ''
-            }
-
-    }]
-
+    DayofWeek: {
+        type: Number,
+        required: [ true, 'El dia de la semana es necesario' ]
+    },
+    DeepTime: {
+        type: Number,
+        required: [ true, 'Es necesarios la hora real de la salida.' ]
+    },
+    CRSDepTime: {
+        type: Number,
+        required: [ true, 'Es necesario la hora de salida programada.' ]
+    },
+    ArrTime: {
+        type: Number,
+        required: [ true, 'Es necesarios la hora real de la llegada.' ]
+    },
+    CRSArrTime:  {
+        type: Number,
+        required: [ true, 'Es necesarios la hora programada de la llegada.' ]
+    },
+    UniqueCarrier:  {
+        type: String,
+        required: [ true, 'Es necesario registrar el código de la aerolínea.' ]
+    },
+    FlightNum: {
+        type: Number,
+        required: [ true, 'Es necesario registrar el número de vuelo.' ]
+    },
+    TailNum: {
+        type: String,
+        required: [ true, 'Es necesario registrar el número de la cola del avión.' ]
+    },
+    ActualElapsedTime: {
+        type: Number,
+        required: [ true, 'Es necesario registrar el tiempo transcurrido.' ]
+    },
+    CRSElapsedTime: {
+        type: Number,
+        required: [ true, 'Es necesario registrar el tiempo transcurrido desde la reserva.' ]
+    },
+    AirTime:  {
+        type: Number,
+        required: [ true, 'Es necesario registrar el tiempo de vuelo.' ]
+    },
+    ArrDelay: {
+        type: Number,
+        required: [ true, 'Tiempo de retraso en la llegada.' ]
+    },
+    DepDelay: {
+        type: Number,
+        required: [ true, 'Tiempo de retraso en la salida.' ]
+    },
+    Origin: {
+        type: String,
+        required: [ true, 'Es origen del vuelo es necesario.' ]
+    },
+    Dest: {
+        type: String,
+        required: [ true, 'Es destino del vuelo es necesario.' ]
+    },
+    Distance: {
+        type: Number,
+        required: [ true, 'La distancia del vuelo en miles es necesaria.' ]
+    },
+    TaxiIn:{
+        type: Number,
+        required: [ true, 'Tiempo en llegar el taxi.' ]
+    },
+    TaxiOut: {
+        type: Number,
+        required: [ true, 'Tiempo de salida del taxi.' ]
+    },
+    Cancelled:  {
+        type: Number,
+        default: 0
+    },
+    CancellationCode:  {
+        type: String,
+        default: "",
+    },
+    Diverted:  {
+        type: Number,
+        default: 0
+    },
+    CarrierDelay: {
+        type: Number,
+        default: 0
+    },
+    WeatherDelay: {
+        type: Number,
+        default: 0
+    },
+    NASDelay: {
+        type: Number,
+        default: 0
+    },
+    SecurityDelay: {
+        type: Number,
+        default: 0
+    },
+    LateAircraftDelay: {
+        type: Number,
+        default: 0
+    }
 });
 
 
 
-export const Clase = model<IClase>('Clase',claseSchema)
+export const Airoport = model<IAiroport>('Airoport',AiroportSchema)
---
+++ src/Interfaces/Airoport.ts
@@ -0,0 +1,33 @@
+import { Document } from "mongoose";
+
+export default interface IAiroport extends Document{
+    Year: String;
+    Month: Number;
+    DayofMonth: Number;
+    DayofWeek: Number;
+    DeepTime: Number;
+    CRSDepTime: Number;
+    ArrTime: Number;
+    CRSArrTime: Number;
+    UniqueCarrier: String;
+    FlightNum: Number;
+    TailNum: String;
+    ActualElapsedTime: Number;
+    CRSElapsedTime: Number;
+    AirTime: Number;
+    ArrDelay: Number;
+    DepDelay: Number;
+    Origin: String;
+    Dest: String;
+    Distance: Number;
+    TaxiIn: Number;
+    TaxiOut: Number;
+    Cancelled: Number;
+    CancellationCode: String;
+    Diverted: Number;
+    CarrierDelay: Number;
+    WeatherDelay: Number;
+    NASDelay: Number;
+    SecurityDelay: Number;
+    LateAircraftDelay: Number;
+}
